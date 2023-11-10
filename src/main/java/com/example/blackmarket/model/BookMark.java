7a7bd2e14909d8be8eff752aa1d3f8e87cb04a91
Glenn <anes53027@gmail.com>
2023-11-10 20:33:03 +0900

    Feat: 북마크 기능 추가 -[x] : 북마크 생성 -[x] : 북마크 조회 -[x] : 유저별 북마크 조회

---
+++ src/main/java/com/example/blackmarket/model/BookMark.java
@@ -0,0 +1,41 @@
+package com.example.blackmarket.model;
+
+
+import com.example.blackmarket.dto.response.BookMarkDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+public class BookMark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Post post;
+
+    @ManyToOne
+    private User user;
+
+    public BookMark(Post post, User user) {
+        this.post = post;
+        this.user = user;
+    }
+
+    public BookMarkDto toDto() {
+        return BookMarkDto.builder()
+                .id(id)
+                .post(post.toDto())
+                .user(user.toDto())
+                .build();
+    }
+}
